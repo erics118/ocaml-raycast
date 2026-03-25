cc4f917018f79634c5f2a13e16897617452062ad
erics118 <52634785+erics118@users.noreply.github.com>
2026-03-25 15:30:52 -0400

    world doesnt need to track objects bc we have bvh

--- lib/world.ml
+++ lib/world.ml
@@ -1,26 +1,15 @@
-type t =
-  { objects : Hittable.t list
-  ; bbox : Aabb.t
-  }
+type t = Hittable.t option
 
 let make = function
-  | [] -> { objects = []; bbox = Aabb.empty }
-  | objects ->
-    let bvh = Bvh.make objects in
-    { objects = [ bvh ]; bbox = bvh.bounding_box }
+  | [] -> None
+  | objects -> Some (Bvh.make objects)
 ;;
 
-let bounding_box world = world.bbox
+let bounding_box = function
+  | None -> Aabb.empty
+  | Some h -> h.Hittable.bounding_box
+;;
 
 let hit_world world ray interval =
-  let rec aux objs closest_so_far acc =
-    match objs with
-    | [] -> acc
-    | obj :: rest ->
-      let test_i = Interval.make (Interval.min interval) closest_so_far in
-      (match obj.Hittable.hit ray test_i with
-       | Some (hr, mat) -> aux rest hr.t (Some (hr, mat))
-       | None -> aux rest closest_so_far acc)
-  in
-  aux world.objects (Interval.max interval) None
+  Option.bind world (fun h -> h.Hittable.hit ray interval)
 ;;
