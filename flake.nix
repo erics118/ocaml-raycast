7277f14c4e22b049fef66c241591b7af8fdbf514
erics118 <52634785+erics118@users.noreply.github.com>
2026-03-25 01:08:25 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "OCaml development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              ocaml
+              opam
+              dune_3
+              ocamlPackages.ocaml-lsp
+              ocamlPackages.ocamlformat
+              ocamlPackages.merlin
+              ocamlPackages.domainslib
+              ocamlPackages.ppx_inline_test
+            ];
+          };
+        };
+    };
+}
